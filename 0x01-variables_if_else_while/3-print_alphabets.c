60cc89e1cf5a8a164630f3a2e2aeab6de4ef04c3
lebaron21 <baron2venise21@gmail.com>
2023-02-17 15:18:57 -0800

    #3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always returns 0
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z' ; c++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
