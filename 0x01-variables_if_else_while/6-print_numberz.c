3daf7419f02a05bb7911bf9226471782b9754f02
lebaron21 <baron2venise21@gmail.com>
2023-02-17 16:01:17 -0800

    #6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	int t = 0;
+
+	for (; t < 10 ; t++)
+		putchar('0' + t);
+	putchar('\n');
+	return (0);
+}
